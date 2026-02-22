0270aaebeb029f1512546575cb15b954365dbc07
axseem <max@axseem.me>
2026-02-22 17:43:09 +0100

    chore(xdg): minor cleanup and add foot to linux xdg

--- modules/home/linux/xdg.nix
+++ modules/home/linux/xdg.nix
@@ -1,13 +1,12 @@
 {...}: {
   xdg.configFile = {
-    # Hyprland
     "hypr/hyprland.conf".source = ../../../config/hypr/hyprland.conf;
-
     "mako".source = ../../../config/mako;
     "rofi".source = ../../../config/rofi;
     "networkmanager-dmenu".source = ../../../config/networkmanager-dmenu;
     "swaylock".source = ../../../config/swaylock;
     "quickshell".source = ../../../config/quickshell;
     "qBittorrent".source = ../../../config/qBittorrent;
+    "foot".source = ../../../config/foot;
   };
 }
