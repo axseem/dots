b437788085ce9d2c0494f1c0e40c3ed390039c42
axseem <max@axseem.me>
2026-02-22 17:43:17 +0100

    style(networking): fix indentation in hosts blocklist

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -6,23 +6,23 @@
     };
 
     extraHosts = ''
-        # Distraction Blocklist
-       127.0.0.1 reddit.com
-       127.0.0.1 www.reddit.com
-       127.0.0.1 old.reddit.com
+      # Distraction Blocklist
+      127.0.0.1 reddit.com
+      127.0.0.1 www.reddit.com
+      127.0.0.1 old.reddit.com
       127.0.0.1 instagram.com
       127.0.0.1 www.instagram.com
-        127.0.0.1 chess.com
-        127.0.0.1 www.chess.com
-        127.0.0.1 lichess.org
-        127.0.0.1 www.lichess.org
-        127.0.0.1 youtube.com
-        127.0.0.1 www.youtube.com
-        127.0.0.1 tiktok.com
-        127.0.0.1 www.tiktok.com
+      127.0.0.1 chess.com
+      127.0.0.1 www.chess.com
+      127.0.0.1 lichess.org
+      127.0.0.1 www.lichess.org
+      127.0.0.1 youtube.com
+      127.0.0.1 www.youtube.com
+      127.0.0.1 tiktok.com
+      127.0.0.1 www.tiktok.com
 
-        # Spyware Blocklist
-        127.0.0.1 vortex.data.microsoft.com
+      # Spyware Blocklist
+      127.0.0.1 vortex.data.microsoft.com
     '';
   };
 }
