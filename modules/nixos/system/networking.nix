446139808602e505f828f0497a88c5b3b3801903
axseem <max@axseem.me>
2026-03-14 19:32:35 +0100

    Relax blocklist for some sites
    Comment out reddit.com, instagram.com, and youtube.com from
    /etc/hosts blocklist while keeping old.reddit.com, chess sites,
    and tiktok blocked

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -7,17 +7,17 @@
 
     extraHosts = ''
       # Distraction Blocklist
-      127.0.0.1 reddit.com
-      127.0.0.1 www.reddit.com
+      #127.0.0.1 reddit.com
+      #127.0.0.1 www.reddit.com
       127.0.0.1 old.reddit.com
-      127.0.0.1 instagram.com
-      127.0.0.1 www.instagram.com
+      #127.0.0.1 instagram.com
+      #127.0.0.1 www.instagram.com
       127.0.0.1 chess.com
       127.0.0.1 www.chess.com
       127.0.0.1 lichess.org
       127.0.0.1 www.lichess.org
-      127.0.0.1 youtube.com
-      127.0.0.1 www.youtube.com
+      #127.0.0.1 youtube.com
+      #127.0.0.1 www.youtube.com
       127.0.0.1 tiktok.com
       127.0.0.1 www.tiktok.com
 
