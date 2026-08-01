5c9f5351996a6b6ce4ee5f519aefcf6457a4a24b
axseem <max@axseem.me>
2026-08-01 22:09:43 +0200

    feat: block reddit and X via extraHosts

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -8,11 +8,16 @@
     extraHosts = ''
       # Addiction Blocklist
       127.0.0.1 www.youtube.com
-      #127.0.0.1 www.reddit.com
+      127.0.0.1 www.reddit.com
       127.0.0.1 news.ycombinator.com
       127.0.0.1 www.chess.com
       127.0.0.1 www.lichess.org
       127.0.0.1 www.instagram.com
+      127.0.0.1 x.com
+      127.0.0.1 www.x.com
+      127.0.0.1 twitter.com
+      127.0.0.1 www.twitter.com
+      127.0.0.1 mobile.twitter.com
 
       # Spyware Blocklist
       127.0.0.1 vortex.data.microsoft.com
