7e4652aa1818415104380f05db583e890839dc5e
axseem <max@axseem.me>
2026-08-02 15:43:46 +0200

    chore(home): drop node versioned wrappers, keep latest node

--- modules/home/common/node.nix
+++ modules/home/common/node.nix
@@ -1,17 +1,5 @@
-{pkgs, ...}: let
-  mkWrapper = name: pkg: bin:
-    pkgs.writeShellScriptBin name ''exec ${pkg}/bin/${bin} "$@"'';
-in {
+{pkgs, ...}: {
   home.packages = [
-    # Default node = latest
     pkgs.nodejs
-
-    # Versioned wrappers (node22, node24, etc.)
-    (mkWrapper "node22" pkgs.nodejs_22 "node")
-    (mkWrapper "node24" pkgs.nodejs_24 "node")
-    (mkWrapper "npm22" pkgs.nodejs_22 "npm")
-    (mkWrapper "npm24" pkgs.nodejs_24 "npm")
-    (mkWrapper "npx22" pkgs.nodejs_22 "npx")
-    (mkWrapper "npx24" pkgs.nodejs_24 "npx")
   ];
 }
