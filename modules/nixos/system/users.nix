c8fa098688c756ec10fd4d160af1d7d217d66f64
axseem <max@axseem.me>
2026-08-02 15:40:05 +0200

    fix(nixos): join real podman group instead of docker

--- modules/nixos/system/users.nix
+++ modules/nixos/system/users.nix
@@ -6,7 +6,7 @@
   users.users.${username} = {
     shell = pkgs.fish;
     isNormalUser = true;
-    extraGroups = ["wheel" "video" "dialout" "networkmanager" "docker" "adbusers" "audio"];
+    extraGroups = ["wheel" "video" "dialout" "networkmanager" "podman" "adbusers" "audio"];
   };
 
   programs.nix-ld.enable = true;
