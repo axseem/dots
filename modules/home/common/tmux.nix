8905fdbbe02cdb1bc98111da46ef2fdcdea5e15f
axseem <max@axseem.me>
2026-08-08 06:39:13 +0200

    feat(tmux): customize prefix and status bar

--- modules/home/common/tmux.nix
+++ modules/home/common/tmux.nix
@@ -4,6 +4,23 @@
     mouse = true;
     terminal = "tmux-256color";
     historyLimit = 100000;
+    extraConfig = ''
+      set -g prefix C-b
+      set -g status off
+      set -g status-style "bg=black,fg=default"
+      set -g window-status-style "bg=black,fg=default"
+      set -g window-status-current-style "bg=black,fg=default"
+      set -g pane-border-style "bg=default,fg=black"
+      set -g pane-active-border-style "bg=default,fg=black"
+
+      set -gu status-left
+      set -gu status-right
+      set -gu window-status-format
+      set -gu window-status-current-format
+      unbind-key -q -T root C-b
+      bind-key -T prefix C-b send-prefix
+      bind-key -T prefix b if-shell -F '#{==:#{status},on}' 'set -g status off' 'set -g status on'
+    '';
   };
 
   programs.fish.interactiveShellInit = lib.mkAfter ''
