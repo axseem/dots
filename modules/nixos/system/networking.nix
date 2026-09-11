ac2efd3bf81014ce739c4300120e5d8b090aeef7
axseem <max@axseem.me>
2026-09-11 13:12:41 +0200

    chore(networking): expand addictive site blocklist

--- modules/nixos/system/networking.nix
+++ modules/nixos/system/networking.nix
@@ -8,10 +8,15 @@
     extraHosts = ''
       # Addiction Blocklist
       #127.0.0.1 www.youtube.com
+      127.0.0.1 reddit.com
       127.0.0.1 www.reddit.com
+      127.0.0.1 old.reddit.com
       127.0.0.1 news.ycombinator.com
+      127.0.0.1 chess.com
       127.0.0.1 www.chess.com
+      127.0.0.1 lichess.org
       127.0.0.1 www.lichess.org
+      127.0.0.1 instagram.com
       127.0.0.1 www.instagram.com
       127.0.0.1 x.com
       127.0.0.1 www.x.com
